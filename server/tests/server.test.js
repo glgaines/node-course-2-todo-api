1a64bc64787d35f78c6b1d48d8bdad0cef85cd27
glgaines <glgaines@gmail.com>
2017-09-19 19:09:03 -0400

    add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text:'first test todo'
+}, {
+  text:' second text todo'
+}];
+
 beforeEach((done) => {
   Todo.remove({})
-    .then(() => done())
+    .then(() => {
+      Todo.insertMany(todos)  // primes the database with the two todos above
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
         if(err) {
           return done(err);
         }
-        Todo.find()
+        Todo.find({text})
           .then((todos) => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -44,9 +52,21 @@ describe('POST /todos', () => {
         }
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           }).catch((e) => done(e));
       })
   })
+});
+
+describe('GET /todos', () => {
+  it('should get all todos (2)', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
