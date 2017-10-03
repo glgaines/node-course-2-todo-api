6d1f6117e65dc4fed5f39cb94bf88991aaf234d6
glgaines <glgaines@gmail.com>
2017-10-03 16:39:31 -0400

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -268,25 +268,43 @@ describe('POST /users/login/', () => {
         }).catch((e) => done(e));
       });
   });
-  it('should reject invalid login', (done) => {
+  // it('should reject invalid login', (done) => {
+  //   request(app)
+  //     .post('/users/login')
+  //     .send({
+  //       email: users[1].email,
+  //       password: users[1].password ='1'
+  //     })
+  //     .expect(400)
+  //     .expect((res) => {
+  //         expect(res.header['x-auth']).toNotExist();
+  //       })
+  //     .end((err, res) => {
+  //       if(err) {
+  //         return done(err);
+  //       }
+  //       User.findById(users[1]._id).then((user) => {
+  //         expect(user.tokens.length).toBe(0);
+  //         done();
+  //       }).catch((e) => done(e));
+  //     })
+  //   });
+});
+describe("DELETE /users/me/token/", () => {
+ it('should remove auth token on logout', (done) => {
+
     request(app)
-      .post('/users/login')
-      .send({
-        email: users[1].email,
-        password: users[1].password ='1'
-      })
-      .expect(400)
-      .expect((res) => {
-          expect(res.header['x-auth']).toNotExist();
-        })
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
       .end((err, res) => {
         if(err) {
           return done(err);
         }
-        User.findById(users[1]._id).then((user) => {
+        User.findById(users[0]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
           done();
         }).catch((e) => done(e));
       })
-    });
+  });
 });
